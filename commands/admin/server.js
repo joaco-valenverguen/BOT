c4d7a551e29b51708208b51fe7c6200a4fbb7a11
Joaco Valenverguen <llerenaluiseduardo3@gmail.com>
2020-10-15 12:43:24 -0500

    comando server mejorado

--- commands/admin/server.js
+++ commands/admin/server.js
@@ -23,7 +23,8 @@ module.exports = class ServerCommand extends Command {
         .addField('Region', server.region, true)
         .addField('Creado el', server.joinedAt.toDateString(), true)
         .addField('Dueño del Servidor', server.owner.user.tag+' ('+server.owner.user.id +')', true)
-        .addField('Miembros', server.memberCount, true)
+        .addField('Miembros', message.guild.members.cache.filter(m => m.user.bot == false).size,true)
+        .addField('bots', message.guild.members.cache.filter(m => m.user.bot == true).size , true)
         .addField('Roles', server.roles.size, true)
         .setColor(0x66b3ff)
             
